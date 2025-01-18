488921e8da194579cc7b7fac607bfaeaa86060e3
Rida <hamdanmadaraa@gmail.com>
2025-01-18 19:33:48 +0500

    Create components: login, register and create organization

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './views/Auth/login/login.component';
+import { RegisterComponent } from './views/Auth/register/register.component';
+import { CreateOrganizationComponent } from './views/Auth/create-organization/create-organization.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login', pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./views/Auth/login/login.component')
+          .then(mod => mod.LoginComponent)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./views/Auth/register/register.component')
+          .then(mod => mod.RegisterComponent)
+    },
+    {
+        path: 'create-organization',
+        loadComponent: () => import('./views/Auth/create-organization/create-organization.component')
+          .then(mod => mod.CreateOrganizationComponent)
+    }
+];
